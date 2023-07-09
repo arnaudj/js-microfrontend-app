00670260ead61d6c8815828817e284ac80639da5
Arnaud J <github@arnaud.dev>
2023-07-09 17:26:33 +0200

    fix(api): disambiguate api version and nonce

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,4 @@
-export const VERSION = 'v1' + Math.floor(Math.random() * 1000000);
+export const VERSION = require('../package.json').version;
+export const NONCE = 'id' + Math.floor(Math.random() * 1000000);
 
 export * from './state';
